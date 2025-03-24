235f4ccdfd02e06c355c37ca78ed8037dfbbca21
max <hmwright404@gmail.com>
2025-03-24 16:28:44 -0500

    part II commit update readme

--- student_app/models.py
+++ student_app/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
-    student_email = models.EmailField(max_length=254, null=False, blank=False)
-    personal_email = models.EmailField(max_length=254, blank=True)
-    locker_number = models.IntegerField(blank=False)
-    locker_combination = models.CharField(max_length=255, blank=False)
+    student_email = models.EmailField(max_length=254, null=False, blank=False, unique=True)
+    personal_email = models.EmailField(max_length=254, blank=True, unique=True)
+    locker_number = models.IntegerField(default=110, unique=True)
+    locker_combination = models.CharField(default="12-12-12", max_length=255)
     good_student = models.BooleanField(default=True, blank=False)
